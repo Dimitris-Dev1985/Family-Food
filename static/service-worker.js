484743f0be375d5545d06037f1d20f4b6f9f523e
Dimitris-Dev1985 <dpap.ee@gmail.com>
2025-07-18 00:06:10 +0300

    Add files via upload

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,28 +1,38 @@
-
-const CACHE_NAME = 'family-food-cache-v1';
+const CACHE_NAME = "family-food-cache-v3";
 const urlsToCache = [
-  '/',
-  '/welcome',
-  '/menu',
-  '/profile',
-  '/history',
-  '/favorites',
-  '/static/icon-192.png',
-  '/static/icon-512.png',
-  '/static/manifest.json'
+  "/",
+  "/welcome",
+  "/static/icon-192.png",
+  "/static/icon-512.png",
+  "/static/manifest.json"
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
+  self.skipWaiting(); // άμεση ενεργοποίηση νέου service worker
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // καθαρισμός παλιών caches
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
