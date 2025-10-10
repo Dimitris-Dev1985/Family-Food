343209bad173db22fa72c6d07c944b6a58501eda
Dimitris-Dev1985 <dpap.ee@gmail.com>
2025-10-10 22:34:06 +0300

    Add files via upload

---
+++ static/service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "family-food-cache-v3";
+const urlsToCache = [
+  "/welcome",
+  "/static/images/icons/icon-192.png",
+  "/static/images/icons/icon-512.png",
+  "/static/manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+  self.skipWaiting(); // άμεση ενεργοποίηση νέου service worker
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // καθαρισμός παλιών caches
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  // 🔎 DEBUG LOG
+  console.log("[SW DEBUG] Fetch:", event.request.url, "Mode:", event.request.mode);
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        console.log("[SW DEBUG] Served from cache:", event.request.url);
+        return response;
+      }
+      console.log("[SW DEBUG] Going to network:", event.request.url);
+      return fetch(event.request);
+    })
+  );
+});
